58b6cc2b70031d7c64625db93367b4b7c7122d41
Matthieu <matthieu.dwle@gmail.com>
2020-05-08 20:22:48 +0200

    :construction: carousel

---
+++ src/carousel/index.js
@@ -0,0 +1,202 @@
+export default class Carousel extends HTMLElement {
+    /**
+     * @callback moveCallback
+     * @param {number} index
+     */
+
+    constructor() {
+        super()
+        this.isMobile = false
+        this.moveCallbacks = []
+        this.previous = this.previous.bind(this)
+        this.next = this.next.bind(this)
+        this.onResize = this.onResize.bind(this)
+        /**/
+        this.items = [].slice.call(this.children)
+        this.innerHTML = ''
+        /**/
+        this.options = this.setOptions()
+        this.createElements()
+        this.items = this.setItems()
+        this.setStyle()
+        this.appendElements()
+        if (this.options.pagination) this.createPagination()
+        /**/
+        this.currentSlide = 0
+        this.moveCallbacks.forEach( cb => cb(this.currentSlide))
+        this.onResize()
+    }
+
+    connectedCallback() {
+        this.carousel__previous.addEventListener('click', this.previous)
+        this.carousel__next.addEventListener('click', this.next)
+        window.addEventListener('resize', this.onResize)
+    }
+    disconnectedCallback () {
+    }
+
+    /**
+     *
+     * @param {moveCallback} cb
+     */
+    onMove (cb) {
+        this.moveCallbacks.push(cb)
+    }
+
+    /**
+     * Check on resize if page pass on mobile format
+     */
+    onResize () {
+        let mobile = window.innerWidth < 900
+        if (mobile !== this.isMobile) {
+            this.isMobile = mobile
+            this.setStyle()
+        }
+    }
+
+    get slidesVisible () {
+        return this.isMobile ? 1 : this.options.slidesVisible
+    }
+
+    get slidesStep () {
+        return this.isMobile ? 1 : this.options.slidesStep
+    }
+
+
+    /**
+     * Show previous slides on click
+     */
+    previous () {
+        this.canGo(this.currentSlide - this.slidesStep)
+    }
+    /**
+     * Show next slides on click
+     */
+    next () {
+        this.canGo(this.currentSlide + this.slidesStep)
+    }
+    /**
+     * Move carousel to the target slide
+     * @param {number} translateX
+     */
+    goToSlide (translateX) {
+        this.carousel__items.style.transform = 'translate3d('+ translateX +'%,0,0)'
+    }
+    /**
+     * Test if can move the carousel items
+     * @param {number} i
+     */
+    canGo (i) {
+        let translateX
+        if ((i < this.items.length - this.slidesVisible) && i >= 0) {
+            translateX = (-i * 100 / this.items.length)
+            this.currentSlide = i
+        }
+        else if (this.items.length - i > 0 && i >= 0 && this.currentSlide !== this.items.length - this.slidesVisible) {
+            translateX = (-(this.items.length - this.slidesVisible) * 100 / this.items.length)
+            this.currentSlide = this.items.length - this.slidesVisible
+        }
+        else if (this.currentSlide !== 0 && (i < this.currentSlide || this.options.backToStart)){
+            translateX = 0
+            this.currentSlide = 0
+        }
+        this.moveCallbacks.forEach( cb => cb(this.currentSlide))
+        this.goToSlide(translateX)
+    }
+    /**
+     * Return all options pass in custom component
+     * @returns {{slidesStep: number, slidesVisible: number}}
+     */
+    setOptions () {
+        return {
+            slidesVisible: parseInt(this.getAttribute('data-slides-visible')),
+            slidesStep: parseInt(this.getAttribute('data-slides-step')),
+            backToStart: this.getAttribute('data-not-back-to-start') === null,
+            pagination: this.getAttribute('data-pagination') !== null
+        }
+    }
+    /**
+     * Resize and place each items in div
+     * @returns {Array} items
+     */
+    setItems () {
+        return this.items.map((item) => {
+            item.setAttribute('style', 'width: 100%; height: 100%; object-fit: cover')
+            let elem = this.createElementWithClass('div', 'carousel__item')
+            elem.appendChild(item)
+            elem.setAttribute('aria-roledescription','slide')
+            elem.setAttribute('role','group')
+            this.carousel__items.appendChild(elem)
+            return elem
+        })
+    }
+    /**
+     * Set size of container and items
+     */
+    setStyle () {
+        this.ratio = this.items.length / this.slidesVisible
+        this.carousel__items.style.width = this.ratio * 100 + '%'
+        this.items.forEach(item => item.style.width = ((100 / this.slidesVisible) / this.ratio) +'%')
+    }
+    /**
+     * Create all carousel elements and store them
+     */
+    createElements () {
+        this.section = this.createElementWithClass('section', 'carousel')
+        this.section.setAttribute('aria-roledescription', 'carousel')
+        this.carousel__container = this.createElementWithClass('div', 'carousel__container')
+        this.carousel__controls = this.createElementWithClass('div', 'carousel__controls')
+        this.carousel__previous = this.createElementWithClass('button', 'carousel__previous carousel__button')
+        this.carousel__previous.setAttribute('aria-roledescription','carousel__items')
+        this.carousel__next = this.createElementWithClass('button', 'carousel__next carousel__button')
+        this.carousel__next.setAttribute('aria-roledescription','carousel__items')
+        this.carousel__items = this.createElementWithClass('div', 'carousel__items')
+        this.onMove(index => {
+            if (index === 0) {
+                this.carousel__previous.classList.add('carousel__previous-hidden')
+            } else {
+                this.carousel__previous.classList.remove('carousel__previous-hidden')
+            }
+            if (index >= this.items.length - this.slidesVisible) {
+                this.carousel__next.classList.add('carousel__next-hidden')
+            } else {
+                this.carousel__next.classList.remove('carousel__next-hidden')
+            }
+        })
+    }
+    /**
+     * Append child all carousel elements
+     */
+    appendElements () {
+        this.carousel__controls.appendChild(this.carousel__previous)
+        this.carousel__controls.appendChild(this.carousel__next)
+        this.carousel__container.appendChild(this.carousel__controls)
+        this.carousel__container.appendChild(this.carousel__items)
+        this.section.appendChild(this.carousel__container)
+        this.appendChild(this.section)
+    }
+    /**
+     * @param {string} tagName
+     * @param {string} className
+     * @return {HTMLElement} elem
+     */
+    createElementWithClass (tagName, className) {
+        let elem = document.createElement(tagName)
+        elem.setAttribute('class', className)
+        return elem
+    }
+    createPagination () {
+        this.paginationLength = this.items.length / this.slidesVisible
+        this.pagination = this.createElementWithClass('div', 'carousel__pagination')
+        this.paginationButton = []
+        for (let i = 0; i <= this.paginationLength; i++) {
+            let elem = this.createElementWithClass('div', `carousel__pagination-button${i === 0 ? ' active' : '' }`)
+            this.paginationButton.push(elem)
+            this.pagination.appendChild(elem)
+        }
+        this.carousel__container.appendChild(this.pagination)
+    }
+}
+if (window.autoDefineComponent !== undefined) {
+    customElements.define('my-carousel', Carousel)
+}
---
+++ src/carousel/index.test.js
@@ -0,0 +1,112 @@
+import '@testing-library/jest-dom'
+import 'expect-puppeteer'
+
+describe('Carousel', () => {
+    beforeAll(async () => {
+        await page.goto('http://127.0.0.1:3000/carousel/')
+    })
+    describe('#Construction', () => {
+        describe('should create carousel elements', () => {
+            it('should create section',async () => {
+                const section = await page.$('section.carousel')
+                expect(section).not.toBeNull()
+            });
+            it('should create carousel__container',async () => {
+                const carousel__container = await page.$('div.carousel__container')
+                expect(carousel__container).not.toBeNull()
+            });
+            it('should create carousel__controls',async () => {
+                const carousel__controls = await page.$('div.carousel__controls')
+                expect(carousel__controls).not.toBeNull()
+            })
+            it('should create carousel__previous',async () => {
+                const carousel__previous = await page.$('button.carousel__previous')
+                expect(carousel__previous).not.toBeNull()
+            })
+            it('should create carousel__next',async () => {
+                const carousel__next = await page.$('button.carousel__next')
+                expect(carousel__next).not.toBeNull()
+            })
+            it('should create carousel__items',async () => {
+                const carousel__items = await page.$('div.carousel__items')
+                expect(carousel__items).not.toBeNull()
+            })
+        })
+        describe('should have attributes',() => {
+            it('should have aria-roledescription', async () => {
+                const carouselCount = await page.$$eval('my-carousel', el => el.length)
+                const carousel = (await page.$$('.carousel[aria-roledescription="carousel"]')).length
+                expect(carousel).toBe(carouselCount)
+                const items = (await page.$$('.carousel__item[aria-roledescription="slide"]')).length
+                expect(items).toBeGreaterThanOrEqual(carouselCount)
+            })
+
+            it('should have valid aria-controls', async () => {
+                const carouselCount = await page.$$eval('my-carousel', el => el.length)
+                console.log(carouselCount)
+                const previous = (await page.$$('.carousel__previous[aria-roledescription="carousel__items"]')).length
+                expect(previous).toBe(carouselCount)
+                const next = (await page.$$('.carousel__next[aria-roledescription="carousel__items"]')).length
+                expect(next).toBe(carouselCount)
+            })
+
+            it('should have role', async  () => {
+                const items = (await page.$$('.carousel__item[role="group"]')).length
+                expect(items).toBeGreaterThanOrEqual(1)
+            })
+        })
+
+        it('should resize images to the carousel',async () => {
+
+        })
+
+        it('should have pagination',async () => {
+
+        })
+
+        it('should be autoplay',async () => {
+
+        })
+
+        it('should not be autoplay', async () => {
+
+        })
+    })
+    describe('#Keyboard-navigation', () => {
+        it('should focus the carousel navigation button', async () => {
+
+        });
+        it('should slide right on space', () => {
+
+        })
+
+        it('should slide left on space', () => {
+
+        })
+        it('should slide right on enter', () => {
+
+        })
+
+        it('should slide left on enter', () => {
+
+        })
+
+    })
+    describe('#Navigation', () => {
+        it('should slide right on click', () => {
+
+        })
+        it('should slide left on click', () => {
+
+        })
+        it('should slide with correct step', () => {
+
+        })
+        it('should stop at the end', () => {
+
+        })
+        it('should be infinite', () => {
+
+        })
+    })
+})
